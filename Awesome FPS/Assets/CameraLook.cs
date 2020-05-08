85e1ef26c78da708a7c5c9adeeab3d8feffa6c04
rbjacob101 <rbjacob101@gmail.com>
2020-05-08 12:53:47 -0600

    Add code for looking around with the camera on the horizontal and vertical axes

---
+++ Awesome FPS/Assets/CameraLook.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+	public float mouseSensitivity;
+	private float cameraRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		float x = mouseSensitivity * Input.GetAxis("Mouse X") * Time.deltaTime;
+		float y = mouseSensitivity * Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+		cameraRotation -= y;
+
+		transform.localRotation = Quaternion.Euler(cameraRotation, 0f, 0f);
+    }
+}
